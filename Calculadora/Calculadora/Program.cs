25f0e330971d8d90e08b65c32161bea3332de145
MatheusSQueiroz <matheussq15@gmail.com>
2023-08-23 11:36:43 -0300

    Aula 02

---
+++ Calculadora/Calculadora/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           float numero1, numero2;
+
+            Console.WriteLine("Digite o 1º número: ");
+            numero1 = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Digite o 2º número");
+            numero2 = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine($"{numero1} + {numero2} = " + (numero1 + numero2));
+            Console.WriteLine($"{numero1} - {numero2} = " + (numero1 - numero2));
+            Console.WriteLine($"{numero1} * {numero2} = " + (numero1 * numero2));
+            Console.WriteLine($"{numero1} / {numero2} = {(numero1 / numero2):F2}");
+            Console.WriteLine($"{numero1} ^ {numero2} = " + Math.Pow(numero1, numero2));
+            Console.WriteLine($"Raiz quadrada de {numero1} = " + Math.Sqrt(numero1));
+            Console.WriteLine($"Raiz quadrada de {numero2} = " + Math.Sqrt(numero2));
+        }
+    }
+}
