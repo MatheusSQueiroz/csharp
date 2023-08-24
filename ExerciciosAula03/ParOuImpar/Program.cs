655140102f3ce6533f3d005baaf40a804f2f2564
MatheusSQueiroz <matheussq15@gmail.com>
2023-08-24 12:37:09 -0300

    Exercicio 02

---
+++ ExerciciosAula03/ParOuImpar/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ParOuImpar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.Write("Digite o primeiro número: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if(num > 0 &&  num %2== 0)
+            {
+                Console.WriteLine($"O número {num} é positivo e par");
+            } 
+            else if(num < 0 &&  num %2!= 0)
+            {
+                Console.WriteLine($"O número {num} é negativo e ímpar");
+            }
+            else if(num < 0 &&  num %2== 0)
+            {
+                Console.WriteLine($"O número {num} é negativo e par");
+            }
+            else if(num > 0 && num %2!= 0)
+            {
+                Console.WriteLine($"O número {num} é positivo e ímpar");
+            }
+        }
+    }
+}
