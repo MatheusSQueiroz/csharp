95ee7cd2c18ecdbf63370bfac166a8d572ce5348
MatheusSQueiroz <matheussq15@gmail.com>
2023-08-25 15:59:55 -0300

    Exercicio 1 laço For

---
+++ Aula04/Exercicios/ExercicioFor/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ExercicioFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+
+            Console.Write("Digite o 1º número: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o 2º número: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            if(num1 < num2)
+            {
+                for(num1 = 0; num1 <= num2; num1++)
+                {
+
+                    if(num1 > 0 && num1 % 3 == 0 && num1 % 5 == 0)
+                    {
+                        Console.WriteLine($"\n{num1} é múltiplo de 3 e 5");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Intervalo inválido!");
+            }
+
+            }
+            
+        }
+    }
