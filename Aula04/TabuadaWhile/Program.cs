53d68329117732f148f191270182e707f1c9e563
MatheusSQueiroz <matheussq15@gmail.com>
2023-08-25 10:10:13 -0300

    Tabuada usando laço While

---
+++ Aula04/TabuadaWhile/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TabuadaWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
