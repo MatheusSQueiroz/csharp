5357d47091161904e7aad92036307ab52a607dda
MatheusSQueiroz <matheussq15@gmail.com>
2023-08-25 15:03:05 -0300

    Exercicio-5 DoWhile

---
+++ Aula04/Exercicios/ExercicioDoWhile/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ExercicioDoWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, somaPositivo = 0;
+
+            do
+            {
+                Console.Write("Digite o número: ");
+                num = Convert.ToInt32(Console.ReadLine());
+
+                if(num > 0)
+                {
+                    somaPositivo += num;
+                }
+
+            
+            }while(num != 0);
+
+                Console.WriteLine($"A soma dos números positivos é: {somaPositivo}");
+        }
+    }
+}
