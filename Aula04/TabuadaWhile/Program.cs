502dac49f8d9f66ee5922aca57e7cc7ae72b7846
MatheusSQueiroz <matheussq15@gmail.com>
2023-08-25 10:23:51 -0300

    Laço DoWhile

--- Aula04/TabuadaWhile/Program.cs
+++ Aula04/TabuadaWhile/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int numero, contador = 1;
+
+            Console.Write("Digite a tabuada desejada: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            while(contador <= 10)
+            {
+                Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+                contador++;
+            }
         }
     }
 }
