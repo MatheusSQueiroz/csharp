5848b4f8d0146bbaef5a317ce473bfa5920fbc8d
MatheusSQueiroz <matheussq15@gmail.com>
2023-08-28 14:02:00 -0300

    Exercicio 01 - Vetor e Matriz

---
+++ Aula05/Exercicios/Exercicio01/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Exercicio01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetorInt = { 2, 5, 1, 3, 4, 9, 7, 8, 10, 6 };
+            int num;
+
+            Array.Sort(vetorInt);
+
+            Console.Write("Digite um número para ser encontrado no vetor: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+               int posicao = Array.BinarySearch(vetorInt, num);
+
+            if (posicao >= 0)
+                Console.WriteLine($"O número {num} está localizado na posição {posicao}");
+
+            else
+                Console.WriteLine($"O número {num} não foi encontrado");
+
+        }
+    }
+}
