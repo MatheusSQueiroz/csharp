ed977d7778a74176582a278349019ddbf9b95c0d
MatheusSQueiroz <matheussq15@gmail.com>
2023-08-29 12:55:41 -0300

    Exercicio HashSet

---
+++ Aula06/Exercicios/Exercicio03-HashSet/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercicio03_HashSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> num = new HashSet<int>();
+
+            for(int i = 1; i <= 10; i++) 
+            {
+                Console.Write("Digite uma número: ");
+                num.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+
+            Console.WriteLine("Dados do HashSet:");
+
+            foreach (var numeros in num)
+            {
+                Console.WriteLine(numeros);
+            }
+        }
+    }
+}
