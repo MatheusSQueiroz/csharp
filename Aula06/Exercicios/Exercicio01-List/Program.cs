1b98bfbe684ec5e594687070696759b90cc78803
MatheusSQueiroz <matheussq15@gmail.com>
2023-08-29 12:38:47 -0300

    Exercicio Collection e Estrutura de dados

---
+++ Aula06/Exercicios/Exercicio01-List/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Exercicio01_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> cores = new List<string>();
+
+
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.Write("Digite uma cor: ");
+                cores.Add(Console.ReadLine());
+
+            }
+                
+                Console.WriteLine("Todas as cores:");
+            
+                foreach (var cor in cores)
+                {
+                    Console.WriteLine(cor);
+                }
+
+                cores.Sort();
+
+                Console.WriteLine("Cores ordenadas:");
+
+                foreach (var cor in cores)
+                {
+                    Console.WriteLine(cor);
+                }
+        }
+    }
+}
