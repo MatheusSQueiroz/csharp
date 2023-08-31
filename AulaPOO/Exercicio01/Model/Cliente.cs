3a8538b1bd9bc54e81ab9a90c0bd27c091e17b0f
MatheusSQueiroz <matheussq15@gmail.com>
2023-08-31 14:57:29 -0300

    Cliente POO

---
+++ AulaPOO/Exercicio01/Model/Cliente.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio01.Model
+{
+    public class Cliente
+    {
+        int id;
+        private string nome = string.Empty;
+        private string cpf = string.Empty;
+        private string cel = string.Empty;
+        private string endereco = string.Empty;
+
+        public Cliente(int id, string nome, string cpf, string cel, string endereco)
+        {
+            this.id = id;
+            this.nome = nome;
+            this.cpf = cpf;
+            this.cel = cel;
+            this.endereco = endereco;
+        }
+        public int GetId()
+        {
+            return id;
+        }
+
+        public void SetId(int id)
+        {
+            this.id = id;
+        }
+
+        public string GetNome()
+        {
+            return nome;
+        }
+
+        public void SetNome(string nome)
+        {
+            this.nome = nome;
+        }
+        public string GetCPf()
+        {
+            return cpf;
+        }
+
+        public void SetCpf(string cpf)
+        {
+            this.cpf = cpf;
+        }
+
+        public string GetCel()
+        {
+            return cel;
+        }
+
+        public void SetCel(string cel)
+        {
+            this.cel = cel;
+        }
+
+        public string GetEndereco()
+        {
+            return endereco;
+        }
+
+        public void SetEndereco(string endereco)
+        {
+            this.endereco = endereco;
+        }
+
+        public void Visualizar()
+        {
+            Console.WriteLine("***************************************");
+            Console.WriteLine("Dados do cliente:");
+            Console.WriteLine("***************************************");
+            Console.WriteLine("Código do cliente: " + this.id);
+            Console.WriteLine("Nome do cliente: " + this.nome);
+            Console.WriteLine("CPF do cliente: " + this.cpf);
+            Console.WriteLine("Telefone do cliente: " + this.cel);
+            Console.WriteLine("Endereço do cliente: " + this.endereco);
+        }
+
+    }
+
+}
---
+++ AulaPOO/Exercicio01/Program.cs
@@ -0,0 +1,16 @@
+﻿using Exercicio01.Model;
+
+namespace Exercicio01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente c1 = new Cliente (123, "Matheus", "40028922", "70707070", "Rua Sete");
+            Cliente c2 = new Cliente(456, "Zé Bug", "12345678", "49227872", "Rua Error 404");
+          
+            c1.Visualizar();
+            c2.Visualizar();
+        }
+    }
+}
